c9e3b4e1bc2f9187cf9df245b4a329e54b0e3e6d
Sajal Garg <sajalgarg9646@gmail.com>
2025-04-21 20:03:38 +0530

    Initial Commit

---
+++ Finance-Assistant/Finance_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_AssistantApp.swift
+//  Finance-Assistant
+//
+//  Created by Sajal Garg on 21/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Finance_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Finance-AssistantTests/Finance_AssistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_AssistantTests.swift
+//  Finance-AssistantTests
+//
+//  Created by Sajal Garg on 21/04/25.
+//
+
+import Testing
+@testable import Finance_Assistant
+
+struct Finance_AssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
